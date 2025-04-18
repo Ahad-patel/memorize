30df852839160257a1b5cbd518b64a45a067c90e
Abdul Ahad <ahadpatel92@gmail.com>
2025-04-18 20:24:36 +0530

    Initial Commit

---
+++ memorize/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by Abdul Ahad on 18/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .foregroundColor(.blue)
+        .padding()
+    }
+}
+
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
+            else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+    }
+}
+
+
+
+
+#Preview {
+    ContentView()
+}
+
---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Abdul Ahad on 18/04/25.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
